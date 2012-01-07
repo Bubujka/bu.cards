e0d7b54ed6c41ec6ec747910779fda52dd9085cd
Aleksei Kaminin <zendzirou@gmail.com>
2012-01-07 22:56:03 +0400

    Added support for ctrl-alt-keys

--- cards_helper.rb
+++ cards_helper.rb
@@ -101,9 +101,38 @@ def basename pth
         File.basename pth
 end
 
+def get_char
+        # return char from input or C-? C-A-?. 
+        key = ""
+        begin
+                  system("stty raw -echo")
+                  f = STDIN.getc
+                  if (1..26) === f
+                          key = "C-" + (f + 96).chr
+                  elsif f == ?\e
+                        c = STDIN.getc
+                        if (1..26) === c
+                                key = "C-A-" + (c + 96).chr
+                        else
+                                key = "A-" + c.chr
+                        end
+                  else
+                          if f >= 208
+                                  key = f.chr + STDIN.getc.chr
+                          else
+                                  key = f.chr
+                          end
+                  end
+        ensure
+                  system("stty -raw echo")
+        end
+        key
+end
+
+
 def char_gets title = ""
   print title
-  get_character.chr.strip
+  get_char.strip
 end
 
 def dmenu arr, intro = "Enter num: "
