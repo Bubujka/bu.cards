9692aafe2cf46314ac84428f0224deb2892b1395
Aleksei Kaminin <zendzirou@gmail.com>
2012-01-08 19:40:38 +0400

    More git commands

--- commands/git.rb
+++ commands/git.rb
@@ -19,3 +19,31 @@ dir_cmd :git_commit do |dir|
         system "cd #{dir}; git commit"
         char_gets
 end
+
+dir_cmd :git_pull do |dir|
+        clear
+        system "cd #{dir}; git pull"
+        char_gets
+end
+
+dir_cmd :git_push do |dir|
+        clear
+        system "cd #{dir}; git push"
+        char_gets
+end
+
+dir_cmd :git_clear do |dir|
+        clear
+        system "cd #{dir}; git ls-files --deleted | xargs git rm" # thx to http://snippets.dzone.com/posts/show/5669
+end
+
+dir_cmd :git_add_clear_commit_push do |dir|
+        clear
+        system "cd #{dir}; git add ."
+        system "cd #{dir}; git ls-files --deleted | xargs git rm"
+        system "cd #{dir}; git commit"
+        system "cd #{dir}; git push"
+        puts
+        puts "Finished".green
+        char_gets
+end
